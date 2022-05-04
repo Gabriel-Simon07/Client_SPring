a643daf15774cdb250cb6f5cda9a822f64c9c85e
Gabriel Simon <gabrielsimon775@gmail.com>
2022-05-03 22:33:28 -0300

    Client class adjusted

--- client/src/main/java/com/devsuperior/client/entities/Client.java
+++ client/src/main/java/com/devsuperior/client/entities/Client.java
@@ -2,8 +2,13 @@ package com.devsuperior.client.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -11,9 +16,14 @@ import javax.persistence.Table;
 public class Client implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Double income;
 	private Instant birthDate;
 	private  Integer children;
@@ -76,4 +86,23 @@ public class Client implements Serializable {
 	public void setChildren(Integer children) {
 		this.children = children;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 }
