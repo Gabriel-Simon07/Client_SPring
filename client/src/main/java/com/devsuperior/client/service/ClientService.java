ff06b1c0733f5374825b8cca56c100b878cc7086
Gabriel Simon <gabrielsimon775@gmail.com>
2022-05-08 17:20:21 -0300

    Client resource, service and properties of h2 has been implemented

---
+++ client/src/main/java/com/devsuperior/client/service/ClientService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.client.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.client.dto.ClientDTO;
+import com.devsuperior.client.entities.Client;
+import com.devsuperior.client.repository.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = clientRepository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+}
